7706dd58ad7e90fff7d9e1e95a0e9641da9d3dd8
Marcelo cerqueira <marcelocerqueira661@gmail.com>
2021-12-17 08:09:18 -0300

    add aula

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-var horas = 11
+var horas = 18
 
-if (horas < 12) {
-  console.log('Bom dia')
+
+if (horas == 12 || horas == 18) {
+  console.log('Voce esta nahora do rosh')
+} else {
+  console.log(' Você ainda nao esta na horas')
 }
+
+// if (horas < 12) {
+//   console.log('Bom dia')
+
+// } else if (horas < 18) {
+//   console.log('boa tarde')
+
+// } else if (horas < 23) {
+
+//   console.log('boa noite')
+// }
